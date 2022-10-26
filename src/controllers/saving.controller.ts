aeb4b86df3fd24d56d4922d2f17abe9feb498695
Dodanim Castillo F <92007308+DodanimJR@users.noreply.github.com>
2022-10-25 19:54:15 -0600

    Update saving.controller.ts

--- src/controllers/saving.controller.ts
+++ src/controllers/saving.controller.ts
@@ -2,11 +2,11 @@ import { Request, Response } from "express";
 import SavingService from "../services/saving.service";
 import { BaseController } from "../types/base.controller";
 class SavingController extends BaseController {
-  getAll(_req: Request | any, res: Response) {
-    this.responseHandler(res, SavingService.getAll(), 200);
+  async getAll(_req: Request | any, res: Response) {
+    this.responseHandler(res, await SavingService.getAll(), 200);
   }
-  create(_req: Request | any, res: Response) {
-    this.responseHandler(res, SavingService.createSaving(_req.body), 201);
+  async create(_req: Request | any, res: Response) {
+    this.responseHandler(res, await SavingService.createSaving(_req.body), 201);
   }
 }
 
