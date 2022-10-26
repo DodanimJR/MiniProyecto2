d02d8e995e4845ff01fe2c2af12a1171145e619b
Dodanim Castillo F <92007308+DodanimJR@users.noreply.github.com>
2022-10-25 19:52:32 -0600

    Update account.controller.ts

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -2,11 +2,11 @@ import { Request, Response } from "express";
 import AccountService from "../services/account.service";
 import { BaseController } from "../types/base.controller";
 class AccountController extends BaseController {
-  getAll(_req: Request | any, res: Response) {
-    this.responseHandler(res, AccountService.getAll(), 200);
+  async getAll(_req: Request | any, res: Response) {
+    this.responseHandler(res, await AccountService.getAll(), 200);
   }
-  create(_req: Request | any, res: Response) {
-    this.responseHandler(res, AccountService.createAccount(_req.body), 201);
+  async create(_req: Request | any, res: Response) {
+    this.responseHandler(res, await AccountService.createAccount(_req.body), 201);
   }
 }
 
