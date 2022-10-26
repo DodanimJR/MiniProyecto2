ecbd55bb3cd7c43d2f1bde88e16d14912ae028a0
Dodanim Castillo F <92007308+DodanimJR@users.noreply.github.com>
2022-10-25 19:53:19 -0600

    Update currency.controller.ts

--- src/controllers/currency.controller.ts
+++ src/controllers/currency.controller.ts
@@ -2,11 +2,11 @@ import { Request, Response } from "express";
 import CurrencyService from "../services/currency.service";
 import { BaseController } from "../types/base.controller";
 class CurrencyController extends BaseController {
-  getAll(_req: Request | any, res: Response) {
-    this.responseHandler(res, CurrencyService.getAll(), 200);
+  async getAll(_req: Request | any, res: Response) {
+    this.responseHandler(res, await CurrencyService.getAll(), 200);
   }
-  create(_req: Request | any, res: Response) {
-    this.responseHandler(res, CurrencyService.createCurrency(_req.body), 201);
+  async create(_req: Request | any, res: Response) {
+    this.responseHandler(res, await CurrencyService.createCurrency(_req.body), 201);
   }
 }
 
