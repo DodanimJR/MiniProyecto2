c57738a427a59c3de6f68c2549d7655150b8acf6
Dodanim Castillo F <92007308+DodanimJR@users.noreply.github.com>
2022-10-25 19:53:47 -0600

    Update payment.controller.ts

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -2,11 +2,11 @@ import { Request, Response } from "express";
 import PaymentService from "../services/payment.service";
 import { BaseController } from "../types/base.controller";
 class PaymentController extends BaseController {
-  getAll(_req: Request | any, res: Response) {
-    this.responseHandler(res, PaymentService.getAll(), 200);
+  async getAll(_req: Request | any, res: Response) {
+    this.responseHandler(res, await PaymentService.getAll(), 200);
   }
-  create(_req: Request | any, res: Response) {
-    this.responseHandler(res, PaymentService.createPayment(_req.body), 201);
+  async create(_req: Request | any, res: Response) {
+    this.responseHandler(res, await PaymentService.createPayment(_req.body), 201);
   }
 }
 
